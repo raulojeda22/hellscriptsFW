ca51c50008219e3a895a573e2307918335168211
Raül Ojeda <raulojeda10g@gmail.com>
2019-03-21 17:07:42 +0100

    Configuració de la base de dades en el .ini

--- backend/models/Connection.class.php
+++ backend/models/Connection.class.php
@@ -1,13 +1,14 @@
 <?
 class Connection{
+
+	private static function getSettings(){
+		return parse_ini_file(dirname(__FILE__).'/config/DB.ini');;
+	}
+
 	public static function connect(){
-		$host = '127.0.0.1';  
-		$user = "hellscripts";                     
-		$pass = "hellscripts";                             
-		$db = "hellscripts";                      
-		$port = 3306;                           
-		
-		$connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+		$settings = self::getSettings();                       
+		$connection = mysqli_connect($settings['host'], $settings['user'], 
+		$settings['pass'], $settings['db'], $settings['port'])or die(mysql_error());
 		return $connection;
 	}
 	public static function close($connection){
